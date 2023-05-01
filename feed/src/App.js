56cfc4258016a322e8b6d00a06f1809c01c322bc
Erikasunshine <erikaandradecruz@gmail.com>
2023-05-01 15:26:40 -0300

    Projeto Estante de Livros

--- feed/src/App.js
+++ feed/src/App.js
@@ -1,3 +1,18 @@
 import './App.css';
 import BotaoMenu from './Componentes/BotaoMenu/Botao.Jsx';
-import Header 
+import Header from './Componentes/Header/Header.Jsx';
+import Livro from './Componentes/Livro/Livro.Jsx';
+import fotos from './Fotos/Foto Livro.jpg';
+
+function App() {
+  return(
+    <div className="App">
+      <Header />
+      <div id='livros'>
+      <Livro imagem={fotos} descricao="Hoje compartilho com voces os meus livros"
+      titulo="Meus Livros" />
+      </div>
+      </div>
+  );
+};
+export default App;
